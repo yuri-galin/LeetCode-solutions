be4eb364670aca3f30cfdc53718adc19799b5092
Yuri Galin <117083475+yuri-galin@users.noreply.github.com>
2022-10-31 15:19:39 +0300

    Update roman-to-integer.py
    Added comments and got rid of code duplication.

--- roman-to-integer.py
+++ roman-to-integer.py
@@ -1,5 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        # As roman numerals use different letters for different orders of magnitude, you basically need to add up all the roman digits to convert
+        # a roman numeral to integer.
+        # The problem is the digraphic digits like IV and CD, because if we just add them up as is, we'll get 6 and 600 instead of expected 4 and 400.
+        # We could choose to try and divide the roman numeral string into parts, but luckily for us it's uneccessary, as romans used the system
+        # where IV equals V minus I and CD equals D minus C, so it all comes down to deciding when to add and when to subtract.
+        
         number_dict = {
             "I": 1,
             "V": 5,
@@ -10,20 +16,23 @@ class Solution:
             "M": 1000
         }
         
+        # We get the letters out of the way by converting them to integers right away.
         numbers = [number_dict[letter] for letter in s]
         
+        # Next, we compare each integer to the next to see which one is greater. If first one is greater, we add the next one to it,
+        # if the next one is greater, we subtract the first one.
+        # The special case is when a digraphic digit follows a digit of the greater order of magnitude: MCM equals 1900, but the rules so far will convert it to 2100.
+        # So, if the first integer is greater than the next, we need to check for digraphic digits and get rid of them by converting them to a single one.
+        # The whole algorithm is as follows.
         i = 0
         while i < len(numbers)-1:
-            if numbers[i] == numbers[i+1]:
-                numbers[i] += numbers[i+1]
-                del numbers[i+1]
-            elif numbers[i] > numbers[i+1]:
+            if numbers[i] > numbers[i+1]:
                 if i+2 <= len(numbers)-1 and numbers[i+2] > numbers[i+1]:
                     numbers[i+2] -= numbers[i+1]
                     del numbers[i+1]
-                else:
-                    numbers[i] += numbers[i+1]
-                    del numbers[i+1]
+            elif numbers[i] >= numbers[i+1]:
+                numbers[i] += numbers[i+1]
+                del numbers[i+1]
             else:
                 numbers[i+1] -= numbers[i]
                 del numbers[i]
