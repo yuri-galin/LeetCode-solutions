31f7f7990faadaca295e6c2873b899d2229c0f83
Yuri Galin <117083475+yuri-galin@users.noreply.github.com>
2022-10-31 17:39:43 +0300

    Update ransom-note.py

--- ransom-note.py
+++ ransom-note.py
@@ -1,4 +1,9 @@
 class Solution:
+    """
+    Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+    Each letter in magazine can only be used once in ransomNote.
+    """
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         # We iterate over each letter in ransomNote, search for it in magazine and if we find it, we cut it out, moving onto the next letter.
         # If we don't find it, we return False.
